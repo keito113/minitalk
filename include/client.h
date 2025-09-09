7a640b75012f501e03cb6af4eaa4abeffb1f5b07
Keito Abe <keitabe@c4r2s15.42tokyo.jp>
2025-09-09 17:52:13 +0900

    server_func

--- include/client.h
+++ include/client.h
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   client.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/02 11:19:28 by keitabe           #+#    #+#             */
-/*   Updated: 2025/09/05 10:28:54 by keitabe          ###   ########.fr       */
+/*   Created: 2025/09/09 17:43:04 by keitabe           #+#    #+#             */
+/*   Updated: 2025/09/09 17:44:52 by keitabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef CLIENT_H
+#define CLIENT_H
 
-# define _POSIX_C_SOURCE 200809L
-
-# include "ft_printf.h"
-# include "libft.h"
-# include <limits.h>
-# include <signal.h>
-# include <sys/types.h>
-# include <unistd.h>
-
-typedef struct s_srv
-{
-	volatile sig_atomic_t	flag;
-	pid_t					sender;
-	unsigned char			cur;
-	int						bit_idx;
-	unsigned char			mb;
-	sig_atomic_t			mb_full;
-}							t_srv;
 
 
 #endif
---
+++ include/server.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 13:43:58 by keitabe           #+#    #+#             */
+/*   Updated: 2025/09/09 14:16:31 by keitabe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# define _POSIX_C_SOURCE 200809L
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <limits.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+typedef enum m_srv
+{
+	BIT0_SIG = SIGUSR1,
+	BIT1_SIG = SIGUSR2,
+	ACK_SIG = BIT0_SIG,
+	HAVE_BYTE = 1u << 0,
+	GOT_NUL = 1u << 1
+
+}							t_m_srv;
+
+typedef struct s_srv
+{
+	volatile sig_atomic_t	flag;
+	pid_t					sender;
+	unsigned char			cur;
+	int						bit_idx;
+	volatile sig_atomic_t	mb;
+	volatile sig_atomic_t	mb_full;
+}							t_srv;
+
+extern t_srv				*g_srv;
+
+#endif
