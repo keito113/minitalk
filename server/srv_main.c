d6a30a381fc020567f03e9527c5dee9820651796
Keito Abe <keitabe@c4r2s15.42tokyo.jp>
2025-09-09 17:54:19 +0900

    server_func

--- server/srv_main.c
+++ server/srv_main.c
@@ -5,45 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/02 11:28:13 by keitabe           #+#    #+#             */
-/*   Updated: 2025/09/05 10:43:38 by keitabe          ###   ########.fr       */
+/*   Created: 2025/09/09 09:46:11 by keitabe           #+#    #+#             */
+/*   Updated: 2025/09/09 17:39:45 by keitabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "server.h"
 
-int	srv_handle_bit(int sig, siginfo_t *si, void *u)
+extern t_srv	*g_srv;
+
+static void	srv_handle(int sig, siginfo_t *si, void *u)
 {
-	int		bit;
 	pid_t	spid;
-	t_srv g_srv;
 
-	if (sig == SIGUSR2)
-		bit = 1;
-	else
-		bit = 0;
-	g_srv = 
+	(void)u;
 	spid = si->si_pid;
+	if (g_srv->sender == 0)
+		g_srv->sender = spid;
+	else if (spid != g_srv->sender)
+	{
+		g_srv->cur = 0;
+		g_srv->bit_idx = 0;
+		g_srv->sender = spid;
+	}
+	g_srv->cur <<= 1;
+	if (sig == BIT1_SIG)
+		g_srv->cur |= 1;
+	g_srv->bit_idx++;
+	if (g_srv->bit_idx == 8)
+	{
+		g_srv->mb = g_srv->cur;
+		g_srv->mb_full = 1;
+		g_srv->cur = 0;
+		g_srv->bit_idx = 0;
+		kill(g_srv->sender, ACK_SIG);
+		g_srv->flag |= HAVE_BYTE | ((g_srv->mb == '\0') * GOT_NUL);
+	}
 }
 
-void	srv_setup_handlers(void)
+static void	srv_setup_handlers(void)
 {
 	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
-	sa.sa_sigaction = srv_handle_bit();
-	sigaction(SIGUSR2, &sa, NULL);
+	sigaddset(&sa.sa_mask, BIT0_SIG);
+	sigaddset(&sa.sa_mask, BIT1_SIG);
+	sa.sa_sigaction = srv_handle;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(BIT0_SIG, &sa, NULL) < 0 || sigaction(BIT1_SIG, &sa,
+			NULL) < 0)
+	{
+		write(2, "[internal] sigaction failed\n",
+			ft_strlen("[internal] sigaction failed\n"));
+		exit(5);
+	}
+}
+
+static void	srv_loop(void)
+{
+	unsigned char	c;
+
+	while (1)
+	{
+		if (!g_srv->mb_full)
+			pause();
+		if (!g_srv->mb_full)
+			continue ;
+		c = (unsigned char)g_srv->mb;
+		g_srv->mb_full = 0;
+		g_srv->flag &= ~HAVE_BYTE;
+		if (c == '\0')
+		{
+			g_srv->flag &= ~GOT_NUL;
+			write(1, "\n", 1);
+			g_srv->sender = 0;
+		}
+		else
+			write(1, &c, 1);
+	}
 }
 
 int	main(void)
 {
-	pid_t	pid;
-
-	pid = getpid();
-	ft_printf("server PID: %d\n", (int)pid);
-	while (1)
-		pause();
+	g_srv = (t_srv *)malloc(sizeof(*g_srv));
+	if (!g_srv)
+	{
+		write(1, "[internal] malloc failed\n", 25);
+		exit(5);
+	}
+	g_srv->flag = 0;
+	g_srv->sender = 0;
+	g_srv->cur = 0;
+	g_srv->bit_idx = 0;
+	g_srv->mb = 0;
+	g_srv->mb_full = 0;
+	ft_printf("server PID: %d\n", (int)getpid());
+	srv_setup_handlers();
+	srv_loop();
+	free(g_srv);
 	return (0);
 }
